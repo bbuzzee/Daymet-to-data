5ed9696960ade38dd3ac2d7c29c380624aa79be8
Ben Buzzee <benbuzzee@gmail.com>
2017-04-17 17:49:19 -0500

    nearly finished!

--- DaymetRShiny/app.R
+++ DaymetRShiny/app.R
@@ -10,6 +10,7 @@ library(zipcode)
 library(devtools)
 library(daymetr) # install_github("khufkens/daymetr")
 library(shinythemes)
+library(plotly)
 data(zipcode)
 
 
@@ -47,36 +48,45 @@ ui <- fluidPage(
    sidebarLayout(
      
       sidebarPanel(
-        
-        dateRangeInput("dates", label = h5(strong("Enter a Date Range")),
-                       start = "2011-01-01", end = "2012-01-01"
-        ),
-        
-        selectInput("id", label = h5(strong("How are locations identified?")), 
-                    choices = list("Zip Code" = 1, "Latitude/Longitude" = 2), 
-                    selected = 1
-        ),
-        
-        checkboxInput('header', 'Column Headers', TRUE
-        ),
-        
-        helpText("Required upload format:"
-        ),
-        
-        imageOutput("image", width = 100, height = 125
-        ),
-        
-        fileInput('file1', 'Choose file to upload',
-                  accept = c(
-                    'text/csv',
-                    'text/comma-separated-values',
-                    '.csv'
-                  )
-        )
-        
+                 
+          dateRangeInput("dates", label = h5(strong("Enter a Date Range")),
+                         start = "2011-01-01", end = "2012-01-01"
+          ),
+          
+          selectInput("id", label = h5(strong("How are locations identified?")), 
+                      choices = list("Zip Code" = 1, "Latitude/Longitude" = 2), 
+                      selected = 1
+          ),
+          
+          checkboxInput('header', 'Column Headers', TRUE
+          ),
+          
+          helpText("Required upload format:"
+          ),
+          
+          imageOutput("image", width = 100, height = 125
+          ),
+          
+          fileInput('file1', 'Choose file to upload',
+                    accept = c(
+                      'text/csv',
+                      'text/comma-separated-values',
+                      '.csv'
+                    )
+          ),
+          
+          width = 3
       ),
+
       
       mainPanel(
+        column(3, uiOutput("selectyr")),
+        column(3, uiOutput("selectsite")),
+        column(3, uiOutput("selectmetric")),
+        br(),
+        br(),
+        br(),
+        plotlyOutput("plot"),
         downloadButton("downloadData", "Download Daymet Data")
       )
    )
@@ -84,11 +94,13 @@ ui <- fluidPage(
 
 
 
+
 # --------------------- Server Code ------------------------------
 # need to modify to input latitude and longitude
 
 server <- function(input, output){
-  
+
+    # read in and modify the data from daymetr
   
     data <- reactive({
       # input$file will be NULL initially. After the user selects
@@ -130,11 +142,64 @@ server <- function(input, output){
 
        dat <- data.frame()
        dat <- do.call(rbind,dat.ls)
+       
+       names(dat) <- c("year", "yday","dayls", "prcp_mm", "srad_wm2", "swe_kgm2",
+                       "tmax_c", "tmin_c", "vpr_pa", "site")
+       
+       # create cumulative growing degree day calculations for each site and for each
+       # year
+       
+       dat <-  dat %>% group_by(year, site) %>% 
+               mutate(gdd_day = ifelse((tmax_c+tmin_c)/2 > 10,(tmax_c + tmin_c)/2 - 10, 0)) %>% 
+               mutate(cumul_gdd = cumsum(gdd_day))
+       
        return(dat)
     })
     
-      
+    # prompt user for year to graph
+    
+    output$selectyr <- renderUI({
+      dat <- data()
+      dat <- unique(dat$year)
+      selectInput(inputId = "yr",label = "Choose a year to graph", dat)
+    })
+    
+    # prompt user for site
+    
+    output$selectsite <- renderUI({
+      dat <- data()
+      dat <- unique(dat$site)
+      selectInput(inputId = "loc",label = "Choose a site", dat)
+    })
+    
+    output$selectmetric <- renderUI({
+      dat <- data()
+      dat <- c("cumul_gdd", "prcp_mm", "srad_wm2", "swe_kgm2",
+               "tmax_c", "tmin_c", "vpr_pa")
+      selectInput(inputId = "metric", label = "Choose a metric", dat)
+    })
 
+    # filter and plot the data
+    
+    output$plot <- renderPlotly({
+      
+      if(is.null(data())){
+        return(NULL)
+      }       
+
+      # m <- list(l = 50,r = 50, b = 100, t = 100,pad = 4)
+      yaxis <- list(title = input$metric)
+      dat <- data()
+      met <- input$metric
+      dat %>% filter(year == input$yr, site == input$loc) %>%
+              plot_ly(x = ~yday, y = ~get(input$metric)) %>% 
+              layout(yaxis = yaxis)
+      
+    })
+    
+    
+    # create download button 
+    
     output$downloadData <- downloadHandler(
       
       filename = function() { 
@@ -142,10 +207,11 @@ server <- function(input, output){
       },
       
       content = function(file) {
-        write.csv(data(), file)
+        write.csv(data(), file, row.names = F)
       })
 
     
+    # render appropriate format image based on input
     
     output$image <- renderImage({
       if(input$header & input$id == 1)
@@ -169,8 +235,6 @@ server <- function(input, output){
 }
     
    
-
-
 
 # Run the application 
 shinyApp(ui = ui, server = server)
